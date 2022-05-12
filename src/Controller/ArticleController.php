6eb9bf993250bf25431a2e851668f11a37cb540f
mahaabid12 <abidmaha921@gmail.com>
2022-05-12 16:12:48 +0200

    routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\MAarticle;
+use App\Form\ArticleType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,4 +21,57 @@ class ArticleController extends AbstractController
             'articles' => $articles ,
         ]);
     }
+
+    #[Route('/article/edit/{id?0} ', name: 'article.edit')]
+    public function addEtudiant(ManagerRegistry $doctrine , Request $request, MAarticle $article=null): Response
+    {
+      
+        if (!$article){
+            $article= new MAarticle();
+        }
+
+        $form=$this->createForm(ArticleType::class , $article); 
+        $form->handleRequest($request);
+        if($form->isSubmitted()){ 
+            //$form->getData()
+            $entityManager = $doctrine->getManager(); 
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+            $this->addFlash("success"," your changes are saved  ");
+            return $this->redirectToRoute('app_article');
+            
+
+        }else{
+            return $this->render('article/form.html.twig', [
+                'form' => $form->createView(),
+            ]);
+
+
+         
+    }
 }
+
+#[Route('/article/remove/{id?0} ', name: 'article.remove')]
+public function remove(ManagerRegistry $doctrine , MAarticle $article=null  ){
+    if ($article ==null){
+        $this->addFlash('error','Article not found'); 
+        return ($this->redirectToRoute('app_article'));
+
+    }else {
+        $manager=$doctrine->getManager(); 
+        //ajout la fonction de suppression dans la transaction 
+        $manager->remove($article);
+        //excecuter la transaction 
+        $manager->flush();
+        $s=$article->getNom();
+        $this->addFlash('success', " $s successfully removed "); 
+        return ($this->redirectToRoute('app_article'));
+
+    } 
+
+}
+
+}
+
+
